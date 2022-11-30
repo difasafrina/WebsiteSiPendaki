7060995d04711552abaf93f37f1b7d58b9a8b9e1
difasafrina <difa.safrina@upi.edu>
2022-11-30 15:16:41 +0700

    first commit

---
+++ resources/views/SiPendaki.blade.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Si Pendaki</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  </head>
+  <body>
+<!-- Ini Bagian Navbar -->    
+    <nav class="navbar navbar-expand-lg" style="background-color: deepskyblue">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Si Pendaki</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Login</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Daftar Gunung</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Dakbar</a>
+                  </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+<!-- Ini Bagian Container 1 -->
+      <div class="container" style="padding-top: 50px">
+        <h1 style="text-align: center">Explore Gunung Indonesia</h1>
+        <p style="text-align: center">Sahabat Pendaki Indonesia</p>
+      </div>
+<!-- Ini Bagian Tab Pencarian -->
+      <div class="container">
+        <div class="row height d-flex justify-content-center align-items-center">
+          <div class="col-md-8">
+            <div class="search" >
+              <i class="fa fa-search"></i>
+              <input type="text" class="form-control" placeholder="Mau mendaki kemana?">
+              <button class="btn btn-primary" style="top:5px; right:5px">Cari</button>
+            </div>            
+          </div>  
+        </div>
+    </div>
+<!-- Ini Bagian Cards -->
+  <div class="container d-flex align-items-center justify-content-center flex-wrap" style="padding-top: 50px">
+  <div class="box">
+      <div class="body">
+          <div class="imgContainer">
+              <img src="https://images.pexels.com/photos/3601422/pexels-photo-3601422.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+          </div>
+          <div class="content d-flex flex-column align-items-center justify-content-center">
+              <div>
+                  <h3 class="text-white fs-5">Post Title</h3>
+                  <p class="fs-6 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sed cum neque, rem provident ex. Laboriosam perspiciatis modi eveniet in?</p>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="box">
+      <div class="body">
+          <div class="imgContainer">
+              <img src="https://images.pexels.com/photos/1532771/pexels-photo-1532771.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+          </div>
+          <div class="content d-flex flex-column align-items-center justify-content-center">
+              <div>
+                  <h3 class="text-white fs-5">Post Title</h3>
+                  <p class="fs-6 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sed cum neque, rem provident ex. Laboriosam perspiciatis modi eveniet in?</p>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="box">
+      <div class="body">
+          <div class="imgContainer">
+              <img src="https://images.pexels.com/photos/573238/pexels-photo-573238.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+          </div>
+          <div class="content d-flex flex-column align-items-center justify-content-center">
+              <div>
+                  <h3 class="text-white fs-5">Post Title</h3>
+                  <p class="fs-6 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sed cum neque, rem provident ex. Laboriosam perspiciatis modi eveniet in?</p>
+              </div>
+          </div>
+      </div>
+  </div>
+  </div>
+<!-- Ini Bagian Testimoni -->
+  <section class="home-testimonial">
+  <div class="container-fluid">
+      <div class="row d-flex justify-content-center testimonial-pos">
+          <div class="col-md-12 d-flex justify-content-center">
+              <h2>Apa Kata Mereka?</h2>
+          </div>
+      </div>
+      <section class="home-testimonial-bottom">
+          <div class="container testimonial-inner">
+              <div class="row d-flex justify-content-center">
+                  <div class="col-md-4 style-3">
+                      <div class="tour-item ">
+                          <div class="tour-desc bg-white">
+                              <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..&rdquo;</div>
+                              <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/6625914/pexels-photo-6625914.jpeg" alt=""></div>
+                              <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
+                              <div class="link-position d-flex justify-content-center">Student</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4 style-3">
+                      <div class="tour-item ">
+                          <div class="tour-desc bg-white">
+                              <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..&rdquo;</div>
+                              <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></div>
+                              <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
+                              <div class="link-position d-flex justify-content-center">Student</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4 style-3">
+                      <div class="tour-item ">
+                          <div class="tour-desc bg-white">
+                              <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..&rdquo;</div>
+                              <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/4946604/pexels-photo-4946604.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></div>
+                              <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
+                              <div class="link-position d-flex justify-content-center">Student</div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+      </section>
+  </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -0,0 +1,6 @@
+    <!-- Bootstrap core JavaScript
+    ================================================= -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></script>
